621a6c466999b0dc6e6b8ffcab7b1217a4536e61
coppy <zheqingli@icloud.com>
2020-01-20 18:01:51 +0800

    release v0.0.1

---
+++ CHANGE_LOG.md
@@ -0,0 +1,6 @@
+# 0.0.1
+- HackRouter api support
+- HackRouter plugin support
+- HackQuery api support
+- useHackQuery hook support
+- createLink api support
--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,23 @@ import { useState } from 'react'
 
 
 /** 创建 onClick 页面链接 */
-export const createHackLink = (url: string, query?: any) =>
+export const createLink = (url: string, query?: any) =>
   () => HackRouter.goTo(url, query)
 
 /** 重定向链接 */
-createHackLink.redirect = (url: string, query?: any) =>
+createLink.redirect = (url: string, query?: any) =>
   () => HackRouter.redirectTo(url, query)
 
 /** tab bar 链接 */
-createHackLink.switchTab = (url: string, query?: any) =>
+createLink.switchTab = (url: string, query?: any) =>
   () => HackRouter.switchTab(url, query)
 
 /** 重启链接 */
-createHackLink.reLaunch = (url: string, query?: any) =>
+createLink.reLaunch = (url: string, query?: any) =>
   () => HackRouter.reLaunch(url, query)
 
 /** 返回链接 */
-createHackLink.back = (delta?: number) =>
+createLink.back = (delta?: number) =>
   () => HackRouter.goBack(delta)
 
 function useHackQuery<Q = any>(): Q {
@@ -32,4 +32,4 @@ function useHackQuery<Q = any>(): Q {
   return hackQuery
 }
 
-export { HackQuery, HackRouter, createHackLink as createHackLink, useHackQuery }
+export { HackQuery, HackRouter, createLink as createLink, useHackQuery }
